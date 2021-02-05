de3c38f99bbd29a9aa27ac9d8898c3c143d3efcb
hp pc <utkarshs1910@gmail.com>
2021-02-05 14:12:55 +0530

    commited changes

--- app/src/main/java/com/londonappbrewery/iampoor/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/iampoor/MainActivity.java
@@ -1,8 +1,8 @@
 package com.londonappbrewery.iampoor;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
